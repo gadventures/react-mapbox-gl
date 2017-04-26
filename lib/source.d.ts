e44dd27a5de7b2b1a44b3c381281d1235107ef19
Justin Barca <jbarca@gadventures.com>
2017-04-26 14:59:29 -0400

    include built files

---
+++ lib/source.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="react" />
+import * as React from 'react';
+import { Map, GeoJSONSource, GeoJSONSourceRaw } from 'mapbox-gl';
+import { TilesJson } from './util/types';
+export interface Context {
+    map: Map;
+}
+export interface Props {
+    id: string;
+    geoJsonSource?: GeoJSONSourceRaw;
+    tileJsonSource?: TilesJson;
+    onSourceAdded?: (source: GeoJSONSource | TilesJson) => any;
+    onSourceLoaded?: (source: GeoJSONSource | TilesJson) => any;
+}
+export default class Source extends React.Component<Props, void> {
+    context: Context;
+    static contextTypes: {
+        map: any;
+    };
+    private id;
+    componentWillMount(): void;
+    private onData;
+    componentWillUnmount(): void;
+    componentWillReceiveProps(props: Props): void;
+    render(): null;
+}
